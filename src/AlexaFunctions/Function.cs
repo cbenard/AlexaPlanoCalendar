3d1832d11433b3531a2c3b5de8750801ea83445a
Chris Benard <chris@chrisbenard.net>
2019-02-24 01:34:26 -0600

    Update SSML to fix responses. Update documentation. Update AWS config now that I understand the security model a bit better.

--- src/AlexaFunctions/Function.cs
+++ src/AlexaFunctions/Function.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Alexa.NET;
 using Alexa.NET.Request;
@@ -54,6 +55,8 @@ namespace AlexaFunctions
                 Text = "Welcome! I can tell you the events that are coming up on the City of Plano Calendar. Just say, when is the next meeting?",
             });
 
+            response.Response.ShouldEndSession = false;
+
             return response;
         }
 
@@ -72,11 +75,10 @@ namespace AlexaFunctions
             string message;
 
             if (intentRequest.Intent.Slots.TryGetValue("calendar_name", out var calendarNameSlot)
-                && !String.IsNullOrWhiteSpace(calendarNameSlot?.Value)
-                && Enum.TryParse(typeof(CalendarType), calendarNameSlot?.Value, out var tempCalendarType)
-                && (CalendarType)tempCalendarType != CalendarType.None)
+                && TryParseCalendarName(calendarNameSlot, out CalendarType? calendarType, logger)
+                && calendarType != CalendarType.None)
             {
-                Calendar calendar = CalendarService.GetCalendar((CalendarType)tempCalendarType)
+                Calendar calendar = CalendarService.GetCalendar(calendarType.Value)
                     .ConfigureAwait(false).GetAwaiter().GetResult();
 
                 CalendarEntry nextEvent = CalendarService.GetFutureCalendarEntries(calendar)
@@ -85,6 +87,8 @@ namespace AlexaFunctions
                 if (nextEvent != null)
                 {
                     string ssml = CreateSsmlForEvent(calendar, nextEvent);
+
+                    logger.Log($"Created response SSML: {ssml}");
 
                     return ResponseBuilder.Tell(new SsmlOutputSpeech
                     {
@@ -107,24 +111,62 @@ namespace AlexaFunctions
             });
         }
 
+        private bool TryParseCalendarName(Slot calendarNameSlot, out CalendarType? calendarType, ILambdaLogger logger)
+        {
+            string calendarTypeText = calendarNameSlot.Value;
+            var firstResolution = calendarNameSlot.Resolution?.Authorities?.FirstOrDefault();
+            var firstResolutionStatusCode = firstResolution?.Status?.Code;
+            var firstResolutionValue = firstResolution?.Values?.FirstOrDefault()?.Value?.Name;
+
+            if (firstResolutionStatusCode == "ER_SUCCESS_MATCH"
+                && !String.IsNullOrEmpty(firstResolutionValue))
+            {
+                calendarTypeText = firstResolutionValue;
+            }
+
+            logger.Log($"Received request for calendar: {calendarNameSlot.Value} - Resolution code: {firstResolutionStatusCode} - Resolution value: {firstResolutionValue} - Resolved to: {calendarTypeText}");
+
+            if (Enum.TryParse(typeof(CalendarType), calendarTypeText, out var tempCalendarType))
+            {
+                calendarType = (CalendarType)tempCalendarType;
+                return true;
+            }
+
+            calendarType = null;
+            return false;
+        }
+
         private string CreateSsmlForEvent(Calendar calendar, CalendarEntry nextEvent)
         {
-            string ssml = $"<s>The next event in the {calendar.Name} calendar is titled: \"{nextEvent.Name}\".</s>"
-                + $"<s>It starts at <say-as interpret-as=\"time\">{nextEvent.Start.ToString("HH:mm")}</say-as> ";
+            string ssml = "<speak>"; 
 
-            ssml += CreateSsmlForStartDate(nextEvent, ssml);
-            ssml += $"<s>It lasts until <say-as interpret-as=\"time\">{nextEvent.End.ToString("HH:mm")}</say-as>.</s>";
+            ssml += $"<s>The next event in the {EscapeSsmlSpeech(calendar.Name)} calendar is titled: \"{EscapeSsmlSpeech(nextEvent.Name)}\".</s>";
+            ssml += $"<s>It starts at <say-as interpret-as=\"time\">{nextEvent.Start.ToString("hh:mm tt")}</say-as> ";
+
+            ssml += CreateSsmlForStartDate(nextEvent);
+            string nextEventEndString = nextEvent.End.ToString("hh:mm tt");
+
+            if (nextEventEndString == "11:59 PM" || nextEventEndString == "12:00 AM")
+            {
+                nextEventEndString = "midnight";
+            }
+
+            ssml += $"<s>It lasts until <say-as interpret-as=\"time\">{nextEventEndString}</say-as>.</s>";
 
             if (!String.IsNullOrWhiteSpace(nextEvent.Description))
             {
-                ssml += $"<s>Here's the description.</s><s>{nextEvent.Description}</s>";
+                ssml += $"<s>Here's the description.</s><s>{EscapeSsmlSpeech(nextEvent.Description)}</s>";
             }
+
+            ssml += "</speak>";
 
             return ssml;
         }
 
-        private string CreateSsmlForStartDate(CalendarEntry nextEvent, string ssml)
+        private string CreateSsmlForStartDate(CalendarEntry nextEvent)
         {
+            string ssml = String.Empty;
+
             if (DateTimeService.NowCentral.Date == nextEvent.Start.Date)
             {
                 ssml += "<emphasis level=\"strong\">today</emphasis>";
@@ -141,6 +183,17 @@ namespace AlexaFunctions
             ssml += ".</s>";
 
             return ssml;
+        }
+
+        internal string EscapeSsmlSpeech(string input)
+        {
+            string escaped = input;
+
+            escaped = escaped.Replace("&", "and");
+
+            escaped = WebUtility.HtmlEncode(escaped);
+
+            return escaped;
         }
     }
 }
